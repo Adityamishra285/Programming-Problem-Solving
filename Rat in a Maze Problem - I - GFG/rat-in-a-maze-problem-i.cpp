eadb1b32b3b00331f0cc8a565b872d0452aecba7
Aditya Mishra <62032671+Adityamishra285@users.noreply.github.com>
2022-04-25 23:07:51 +0530

    Added solution - LeetHub

--- Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.cpp
+++ Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.cpp
@@ -11,6 +11,33 @@ using namespace std;
 class Solution{
     public:
     
+    /*
+    Remember that in question that you have done in DP notebook (maze with obstecles) their are
+    only 2 paths (Down and Right) so agar tum soch rhe ho ki usme Visited array kyu nhi use hua
+    to uska reason ye hai ki agar tm Ek baar Down gye to tm us down element se Up (arent)
+    kabhi jaa hi nhi skte. 
+    mtlb sirf down and right jaa skte to opposite jaa hi nhi skte to us path ko
+    wapas traverse kr hi nhi skte, but is question me all 4 direction jaa skte, isiliye ek visited
+    array hai jo track rkh rha (LIKE DFS) kyuki idhr agar tm Down gye to, us down wale element se
+    UP wapas usi parent wale pe jaa kste ho (jisse infinite loop hogi (DFS without VISITED array))
+    kyuki UP allowed hai (opposite direction).
+    
+    Concept:
+    Simple hai, tm boundry conditions check kr rhe, ki jo i&j hai (rows and col) wo n-1 se chota
+    hona chhiye and 0 se bada. phir tm main me sidha check kr rhe ki agar staring index 
+    and (n-1)(n-1) 1 hai ki nhi, kyuki agar 1 nhi hoga to tm kbhi destination reach hi 
+    nhi kr skte (think)
+    Tb last me tm func call kr rhe, jo ek baar Down, left, right, Up jaa rha, cause DLRU 
+    lexo sorted hai. phir tm har direction me dfs call kr rhe. Phir usko visit kr rhe or agar 
+    us path se kahi or nhi jaa skte tb tm backtrack kr rhe after reaching (n-1,n-1). 
+    Backtrack krte waqt tm vis wale cell ko wapas se unVis kr rhe kyuki ho skta ki us cell 
+    se kisi or direction me jaane se koi or path possible ho jaise example ka 3,0 cell se backtrack
+    kra to 2,0 pe gye or 3,0 ko unVIs kr diya, to ab jab 2,0 se Down gye to to ab right jaane ka 
+    turn hai (left not possible) to ab new path banega and niche wale saare unVis ho gye honge
+    backtrack krte waqt.
+    path possible ho to us cell ko
+    */
+    
     void fun(int i, int j, vector<vector<int>> &m, int n, vector<string> &res, string str, vector<vector<int>>&vis){
         if(i==n-1 && j==n-1){
             res.push_back(str);
