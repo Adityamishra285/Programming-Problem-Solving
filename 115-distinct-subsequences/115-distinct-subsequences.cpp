ec2639e43b6f6070e621c1a7a8081709ad3fe12d
Aditya Mishra <62032671+Adityamishra285@users.noreply.github.com>
2022-07-02 00:20:57 +0530

    Time: 98 ms (12.73%), Space: 82.9 MB (5.02%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -1,17 +1,27 @@
 class Solution {
 public:
-	int solve(int i,int j,string& s,string& t,vector<vector<int>>& dp){
-		if(j<0) return 1;
-		if(i<0) return 0;
-		
-		if(dp[i][j]!=-1) return dp[i][j];
-			
-		if(s[i]==t[j]) return dp[i][j] = solve(i-1,j-1,s,t,dp) + solve(i-1,j,s,t,dp);
-		else return dp[i][j] = solve(i-1,j,s,t,dp);
-	}
-	int numDistinct(string s, string t) {
-		int n = s.size(), m = t.size();
-		vector<vector<int>> dp(n,vector<int>(m,-1));
-		return solve(n-1,m-1,s,t,dp);
-	}
+    
+    int f(int i, int j, string s, string t, vector<vector<int>> &dp){
+        
+        if(i<j) return 0;
+        if(j<0) return 1;
+        if(i<0) return 0;
+        
+        if(dp[i][j]!=-1) return dp[i][j];
+        
+        if(s[i]==t[j]) return dp[i][j] = f(i-1, j-1, s, t, dp) + f(i-1, j, s, t, dp);
+        
+        else return dp[i][j] = f(i-1, j, s, t, dp);
+    }
+    
+    int numDistinct(string s, string t) {
+        int n = s.size();
+        int m = t.size();
+        
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        
+        return f(n-1, m-1, s, t, dp);
+        
+    }
+
 };
