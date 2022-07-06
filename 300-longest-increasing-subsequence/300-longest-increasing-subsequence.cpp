4ef06451c5167ce4e2c94c6cc356785cf54c9fc4
Aditya Mishra <62032671+Adityamishra285@users.noreply.github.com>
2022-07-07 02:49:55 +0530

    Time: 1147 ms (9.90%), Space: 292.2 MB (6.61%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
     
-    int f(int ind, int prev_ind, int n, vector<int> &nums, vector<vector<int>> &dp){
-        if(ind==n)
-            return 0;
-        
-        
-        if(dp[ind][prev_ind+1]!=-1)
-            return dp[ind][prev_ind+1];
-        int notpick = 0 + f(ind+1, prev_ind, n, nums, dp);
-        int pick = 0;
-        
-        if(prev_ind==-1 || nums[ind]>nums[prev_ind]){
-            pick = 1 + f(ind+1, ind, n, nums, dp);
-        }
-        
-        return dp[ind][prev_ind+1] = max(pick, notpick);
-    }
-    
     int lengthOfLIS(vector<int>& nums) {
         
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n+1, -1));
+        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
         
-        return f(0, -1, n, nums, dp);
+        for(int ind = n-1;ind>=0;ind--){
+            for(int prev = ind-1; prev>=-1;prev--){
+                int notpick = 0 + dp[ind+1][prev+1];
+                int pick = 0;
+                if(prev==-1 || nums[ind]>nums[prev]){
+                    pick = 1 + dp[ind+1][ind+1];
+                }
+                dp[ind][prev+1] = max(pick, notpick);
+            }
+        }
+        
+        return dp[0][0];
     }
 };
